c350fe8e4fbb3601896e045999568e25ee630899
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 17:30:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,6 @@ def key_for_min_value(name_hash)
   key_with_min_value = nil
   min_value = 1000
   name_hash.collect.with_index { |(key,value), index|
-    values << value
       if(min_value > value)
         min_value = value
       end
